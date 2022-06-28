6ea6a6c9df86f93e04376f59ac0c90893e7681c5
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2022-06-28 20:56:35 +0900

    Add quick-paint

---
+++ keyboards/yushakobo/quick_paint/config.h
@@ -0,0 +1,134 @@
+/*
+Copyright 2022 sekigon-gonnoc
+Copyright 2022 Yushakobo
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID    0x3265
+#define PRODUCT_ID   0x0009
+#define DEVICE_VER   0x0001
+#define MANUFACTURER yushakobo
+#define PRODUCT      Quick Paint
+
+/* key matrix size */
+#define MATRIX_ROWS 3
+#define MATRIX_COLS 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 32
+
+// #define DEBUG_MATRIX_SCAN_RATE
+#define DEBUG_ACTION
+
+
+// clang-format off
+#define DIRECT_PINS                 \
+    {                               \
+        { 28, 27, 26, 25, 11 },     \
+        { 24, 23, 22, 21, NO_PIN }, \
+        { 20, 19, 18, 17, NO_PIN }  \
+    }
+
+#define ENCODER_COUNT 1
+
+#define ENCODERS_PAD_A { 12 }
+#define ENCODERS_PAD_B { 13 }
+
+// map encoder to matrix (col, row)
+#define ENCODERS_CCW_KEY { { 4, 1 } }
+#define ENCODERS_CW_KEY  { { 4, 2 } }
+
+// clang-format on
+
+#define RGB_DI_PIN 29
+#ifdef RGB_DI_PIN
+#    define RGBLED_NUM 12
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#    define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
+#    define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+/*== all animations enable ==*/
+#    define RGBLIGHT_ANIMATIONS
+/*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
+#    define RGBLIGHT_BREATHE_TABLE_SIZE 256  // 256(default) or 128 or 64
+/*==== use exp() and sin() ====*/
+#    define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
+#    define RGBLIGHT_EFFECT_BREATHE_MAX 255      // 0 to 255
+#endif
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* If defined, GRAVE_ESC will always act as ESC when CTRL is held.
+ * This is useful for the Windows task manager shortcut (ctrl+shift+esc).
+ */
+//#define GRAVE_ESC_CTRL_OVERRIDE
+
+/*
+ * Force NKRO
+ *
+ * Force NKRO (nKey Rollover) to be enabled by default, regardless of the saved
+ * state in the bootmagic EEPROM settings. (Note that NKRO must be enabled in the
+ * makefile for this to work.)
+ *
+ * If forced on, NKRO can be disabled via magic key (default = LShift+RShift+N)
+ * until the next keyboard reset.
+ *
+ * NKRO may prevent your keystrokes from being detected in the BIOS, but it is
+ * fully operational during normal computer usage.
+ *
+ * For a less heavy-handed approach, enable NKRO via magic key (LShift+RShift+N)
+ * or via bootmagic (hold SPACE+N while plugging in the keyboard). Once set by
+ * bootmagic, NKRO mode will always be enabled until it is toggled again during a
+ * power-up.
+ *
+ */
+//#define FORCE_NKRO
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+/* disable these deprecated features by default */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
+/* Bootmagic Lite key configuration */
+//#define BOOTMAGIC_LITE_ROW 0
+//#define BOOTMAGIC_LITE_COLUMN 0
---
+++ keyboards/yushakobo/quick_paint/readme.md
@@ -0,0 +1,19 @@
+# Quick Paint
+
+* Keyboard Maintainer: [yushakobo](https://github.com/yushakobo)
+* Hardware Supported: quick-paint PCB, RP2040
+* Hardware Availability: [yushakobo Shop](https://shop.yushakobo.jp)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make yushakobo/quick_paint:vial
+
+Flashing example for this keyboard by `picotool`:
+
+    make yushakobo/quick_paint:default:flash
+
+Convert firmware into uf2 format:
+
+    make yushakobo/quick_paint:vial:uf2
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
