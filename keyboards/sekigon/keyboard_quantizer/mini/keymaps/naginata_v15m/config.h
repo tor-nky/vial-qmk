0c2c371daa43faecc7b29727ce5f744350b9db66
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2024-02-17 09:31:58 +0900

    config.hの役割分担

--- keyboards/sekigon/keyboard_quantizer/mini/keymaps/naginata_v15m/config.h
+++ keyboards/sekigon/keyboard_quantizer/mini/keymaps/naginata_v15m/config.h
@@ -33,10 +33,5 @@
 
 // #define NG_USE_KAWASEMI // Mac汎用 → かわせみ専用
 
-#ifdef NG_USE_KAWASEMI
-// Keycodes used for starting Unicode input on different platforms
-#   define UNICODE_KEY_MAC KC_NO   // かわせみ3用
-#endif
-
 // Forces the keyboard to wait for a USB connection to be established before it starts up
 #define WAIT_FOR_USB
