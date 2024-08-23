c3aa6eac64a47b17bd84045534418c3f263ac06a
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2024-08-23 21:28:06 +0900

    GPK60-46SGRのVIAL関係を修正

--- keyboards/daraku_neko/gpk60_46sgr/keymaps/naginata_v15m/rules.mk
+++ keyboards/daraku_neko/gpk60_46sgr/keymaps/naginata_v15m/rules.mk
@@ -20,5 +20,9 @@ CFLAGS += -DGIT_DESCRIBE=$(GIT_DESCRIBE)
 # Override raw_hid_receive to support both of VIA and VIAL
 $(BUILD_DIR)/obj_daraku_neko_gpk60_46sgr_naginata_v15m/quantum/dynamic_keymap.o:: CFLAGS += -Ddynamic_keymap_macro_send=dynamic_keymap_macro_send_vial
 
-# OLED_ENABLE = yes
 OS_DETECTION_ENABLE = yes
+
+RING_BUFFERED_6KRO_REPORT_ENABLE = yes
+	# USB 6-Key Rollover - Instead of stopping any new input once 6 keys
+	# are pressed, the oldest key is released and the new key is pressed.
+DEBOUNCE_TYPE = sym_defer_pk	# debouncing per key.
