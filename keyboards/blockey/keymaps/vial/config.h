5406733d14f5839bc050b62c6310607a52e21cb8
Satoru NAKAYA <HKR7Fm8P7U@gmail.com>
2025-11-24 17:48:49 +0900

    Blockey にRP2040を

---
+++ keyboards/blockey/keymaps/vial/config.h
@@ -0,0 +1,29 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x12, 0x8F, 0x78, 0x63, 0x81, 0xAE, 0xB6, 0xC6}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {5, 6}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define WEAR_LEVELING_BACKING_SIZE (8192 * 2)
+#define WEAR_LEVELING_LOGICAL_SIZE 8192
---
+++ keyboards/blockey/keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+
+BOOTMAGIC_ENABLE = yes  # Enable Bootmagic Lite
+COMMAND_ENABLE = no     # Commands for debug and configuration
+CONSOLE_ENABLE = no     # Console for debug
+MOUSEKEY_ENABLE = yes   # Mouse keys
+NKRO_ENABLE = yes       # USB N-Key Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
